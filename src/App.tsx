66f1cb6f9e81222e337b93191688830225177208
Ken Thore <kenthore@live.no>
2023-11-11 01:43:23 +0100

    added fonts, and added dynamic color switching on slider for dark/light mode

--- src/App.tsx
+++ src/App.tsx
@@ -180,8 +180,8 @@ function LightDarkMode() {
     }
   } else if (isChecked === false) {
     document.documentElement.classList.remove("dark");
-    // rootElement.style.backgroundImage =
-    //   "url(../src/assets/background-light.jpg)";
+    rootElement.style.backgroundImage =
+      "url(../src/assets/background-dark.jpg)";
     rootElement.style.backgroundColor = "rgba(255, 246, 248, 0.9)";
     rootElement.style.backgroundBlendMode = "overlay";
   }
@@ -211,7 +211,7 @@ function LightDarkMode() {
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fill="#D97E96"
+          fill={isChecked ? "#D97E96" : "#025E73"}
           d="M12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"
         />
       </svg>
